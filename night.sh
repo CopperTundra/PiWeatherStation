c366b0d2eaef1042ccc966f3a0d6f1cdb8aee486
CopperTundra <o.schuwer@gmail.com>
2022-12-15 18:33:13 +0100

    Updated night script

--- night.sh
+++ night.sh
@@ -18,5 +18,5 @@
 #!/bin/bash
 
 echo "Disabling services and saving power for the night..."
-systemctl stop piclock
-cp /home/pi/PiClock/off /sys/class/backlight/rpi_backlight/bl_power
+systemctl stop PiWeatherStation
+cp /home/pi/PiWeatherStation/off /sys/class/backlight/rpi_backlight/bl_power
